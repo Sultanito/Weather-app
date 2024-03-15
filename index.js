ad01c6b46efda178f5442325cdd707e4794d99c0
Sultanito <sulter731@gmail.com>
2024-03-15 06:05:45 +0600

    15 v3

---
+++ index.js
@@ -0,0 +1,10 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "b4ca4149011428b62982b864709be977";
+
+weatherForm.addEventListener("submit", event =>{
+
+})
+
+async function getWeatherData()
