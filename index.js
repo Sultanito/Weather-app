1b929132b188cc662b194a229846a2c661f952a0
Sultanito <sulter731@gmail.com>
2024-03-22 14:55:40 +0600

    march 22

--- index.js
+++ index.js
@@ -3,14 +3,66 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "b4ca4149011428b62982b864709be977";
 
-weatherForm.addEventListener("submit", event =>{
+weatherForm.addEventListener("submit", async event =>{
 
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please enter a city");
+    }
 })
 
 async function getWeatherData(city){
     
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch (apiUrl);
+
+    console.log(response);
+
+    if(!reponse.ok){
+        throw new Error("Could not fetch weather data");
+    }
+
+    return await response.json();
 }
 
-function DisplayWeatherInfo(data){
+function displayWeatherInfo(data){
     
+    const {
+        name: city,
+        main: {temp, humidity},
+        weather: [{description, id}]} = data;
+
+    card.textContent = "";
+    card.style.display = "flex";
+
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+    
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 }
