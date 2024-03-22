afb17b95f069b056748cc611f716a2bf744eba09
Sultanito <sulter731@gmail.com>
2024-03-22 15:43:53 +0600

    updated emojis

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// WEATHER APP
+
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
@@ -75,19 +77,19 @@ function getWeatherEmoji(weatherId){
 
     switch(true){
         case (weatherId >= 200 && weatherId < 300):
-            return "⛈";
+            return "⛈️";
         case (weatherId >= 300 && weatherId < 400):
-            return "🌧";
+            return "🌧️";
         case (weatherId >= 500 && weatherId < 600):
-            return "🌧";
+            return "🌧️";
         case (weatherId >= 600 && weatherId < 700):
-            return "❄";
+            return "❄️";
         case (weatherId >= 700 && weatherId < 800):
-            return "🌫";
+            return "‍🌫️";
         case (weatherId === 800):
-            return "☀";
+            return "☀️";
         case (weatherId >= 801 && weatherId < 810):
-            return "☁";
+            return "☁️";
         default:
             return "❓";
     }
